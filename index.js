d954347eadb9b584bb27c5d0177f6360c57ba20e
Brendan Molloy <brendan@bbqsrc.net>
2015-08-04 14:19:26 +1000

    let's make the logging waaaay more flexible

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict';
 
+var util = require('util');
+
+function parseArgs(args) {
+  var o = {};
+
+  var len = args.length;
+  if (len > 0) {
+    if (args[args.length-1] && args[args.length-1].stack) {
+      o.err = args.pop();
+      len--;
+    }
+  }
+
+  if (len === 1 && util.isObject(args[0])) {
+    // JSONable
+    o.data = args[0];
+  } else if (len >= 1) {
+    o.message = args.map(function(arg) {
+      return util.isPrimitive(arg) ? arg : util.inspect(arg);
+    }).join(' ');
+  }
+
+  return o;
+}
+
 function createLog() {
   var Log = {
     formatters: [],
@@ -12,15 +37,18 @@ function createLog() {
       this.formatters.push(fmt);
     },
 
-    println: function(priority, tag, msg, tr) {
+    println: function(priority, tag /*, varargs */) {
       if (priority < this.level) {
         return;
       }
 
+      var args = Array.prototype.constructor.apply(null, arguments).slice(2);
+      args = parseArgs(args);
+
       var ts = new Date;
       var len = Log.formatters.length;
       for (var i = 0; i < len; ++i) {
-        this.formatters[i].call(this, ts, priority, tag, msg, tr);
+        this.formatters[i].call(this, ts, priority, tag, args);
       }
     }
   };
@@ -48,35 +76,34 @@ function createLog() {
 
 var Log = createLog();
 
-var ConsoleFormatter = function(ts, prio, tag, msg, tr) {
+var ConsoleFormatter = function(ts, prio, tag, args) {
   /*eslint-disable no-console */
   var p = [undefined,undefined,'V','D','I','W','E','A'];
-
-  if (msg.stack) {
-    tr = msg;
-    msg = '';
-  }
 
   switch (prio) {
     case Log.VERBOSE:
     case Log.DEBUG:
     case Log.INFO:
-      console.log(ts.toISOString() + ' [' + p[prio] + '] ' + tag + ': ' + msg);
-      if (tr) {
-        console.log(tr.stack);
+      if (args.message) {
+        console.log(ts.toISOString() + ' [' + p[prio] + '] ' + tag + ': ' + args.message);
+      }
+      if (args.err) {
+        console.log(args.err.stack);
       }
       break;
     case Log.WARN:
     case Log.ERROR:
     case Log.ASSERT:
-      console.error(ts.toISOString() + ' [' + p[prio] + '] ' + tag + ': ' + msg);
-      if (tr) {
-        console.error(tr.stack);
+      if (args.message) {
+        console.error(ts.toISOString() + ' [' + p[prio] + '] ' + tag + ': ' + args.message);
+      }
+      if (args.err) {
+        console.error(args.err.stack);
       }
       break;
     default:
-      Log.wtf('ConsoleFormatter', 'invalid priority specified: ' + prio + ', logging as error.');
-      Log.e(tag, msg, tr);
+      Log.wtf('ConsoleFormatter', 'invalid priority specified: ', prio, ', logging as error.');
+      Log.e(tag, args);
   }
 };
 
